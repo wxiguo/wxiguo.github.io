13d871103df92b758f928a1afeaaa60295a28137
wangxiguo <wxq9375@gmail.com>
2019-07-16 14:28:10 +0800

    gulpfile.js htmlmin false

--- gulpfile.js
+++ gulpfile.js
@@ -97,8 +97,8 @@ gulp.task('compressHtml', function () {
     }
     var minOption = {
         collapseWhitespace: false,           //压缩HTML
-        collapseBooleanAttributes: true,    //省略布尔属性的值  <input checked="true"/> ==> <input />
-        removeEmptyAttributes: true,        //删除所有空格作属性值    <input id="" /> ==> <input />
+        collapseBooleanAttributes: false,    //省略布尔属性的值  <input checked="true"/> ==> <input />
+        removeEmptyAttributes: false,        //删除所有空格作属性值    <input id="" /> ==> <input />
         removeScriptTypeAttributes: true,   //删除<script>的type="text/javascript"
         removeStyleLinkTypeAttributes: true,//删除<style>和<link>的type="text/css"
         removeComments: true,               //清除HTML注释
