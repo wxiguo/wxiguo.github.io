40685611a264f9f521bf992adf9748421e88dd7f
wangxiguo <wxq9375@gmail.com>
2019-07-28 11:33:15 +0800

    travis-ci coding

--- publish-to-gh-pages.sh
+++ publish-to-gh-pages.sh
@@ -21,4 +21,4 @@ git commit -m "Travis CI Auto Builder at `date +"%Y-%m-%d %H:%M"`"
 # Github Pages
 git push --force --quiet "https://${TravisCIToken}@${GH_REF}" master:master
 # Coding Pages
-git push --force --quiet "https://wxiguo:eead5a9cc0e7279fb7a86b0a47eeee61b1c80afc@${CO_REF}" master:master
+git push --force --quiet "https://wxiguo:${TravisCOToken}@${CO_REF}" master:master
